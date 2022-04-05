06c1bbb51d24c0f28b26cc4b2396ec09ba2df213
academiapython <academiapython.py@gmail.com>
2022-04-05 20:06:13 -0300

    #05 Quinto Checkpoint. Criação de exemplos bootstrap.

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'exemplos.apps.ExemplosConfig',
     'ckeditor',
     'ckeditor_uploader',
 ]
--- exemplos/urls.py
+++ exemplos/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path('exemplos/', include('exemplos.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('', views.get_bootstrap, name='bootstrap')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
