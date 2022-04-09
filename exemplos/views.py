a20e03b13b84b910cef49c8c0ef2c4619e5fce01
academiapython <academiapython.py@gmail.com>
2022-04-08 22:32:19 -0300

    #Sexto Checkpoint. Refactoring da página inicial, parte I.

--- exemplos/views.py
+++ exemplos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def get_bootstrap(request):
-    return render(request, 'exemplos/12_utilitarios.html')
+    return render(request, 'exemplos/15_display_flex.html')
