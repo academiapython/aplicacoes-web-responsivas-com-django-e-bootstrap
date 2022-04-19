07ee8e0e5b32f5cb19c8f64f00b9776b210e2410
academiapython <academiapython.py@gmail.com>
2022-04-19 20:23:06 -0300

    #11 Checkpoint. 1. Refactoring do template post_detail. 2. Ajustes nos arquivos de urls.

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('blog/', views.blog, name='blog'),
     path('posts/<int:post_id>/post_detail', views.post_detail, name='post_detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('exemplos/', include('exemplos.urls')),
+    path('login/', include('autenticacao.urls')),
+    path('contato/', include('contato.urls')),
+    path('cursos/', include('cursos.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
