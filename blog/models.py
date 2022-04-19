f256cf962068c7ada5ff19ac5db3b0a762dc6e90
academiapython <academiapython.py@gmail.com>
2022-04-19 20:18:12 -0300

    #Sétimo Checkpoint. 1. Refactoring da página inicial. 2. Criação de Fragmentos (carrosel, navbar, footer). 3. Criação de CSS externo.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 from django.utils import timezone
 from ckeditor_uploader.fields import RichTextUploadingField
@@ -8,7 +9,9 @@ class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
+    resumo = RichTextField(blank=True, null=True)
     conteudo = RichTextUploadingField()
+    imagem_capa = models.ImageField(null=True, blank=True, upload_to='static/blog/')
     data_publicacao = models.DateTimeField(timezone.now())
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Post
 
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-data_publicacao')[:5]
     context = {
         'posts': posts
     }
@@ -15,3 +15,10 @@ def post_detail(request, post_id):
         'post': post
     }
     return render(request, 'blog/post_detail.html', context)
+
+def blog(request):
+    posts = Post.objects.order_by('-data_publicacao')[:5]
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog.html', context)
