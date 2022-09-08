a4f080aeb9f2f6ae92ab54e33628781298e70718
academiapython <academiapython.py@gmail.com>
2022-09-08 18:19:48 -0300

    Retornando arquivos ao estado original.

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.get_quiz, name='get_quiz'),
-    path('quiz/resposta/', views.get_resposta, name='resposta')
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -12,14 +12,3 @@ def get_quiz(request):
     }
     return render(request, 'quiz/quiz.html', context)
 
-
-def get_resposta(request):
-
-    resposta = request.POST.get('resposta')
-
-    if resposta == 'True':
-        return HttpResponse("<p style='color:green'> Resposta Correta! </p>")
-    elif resposta == 'False':
-        return HttpResponse("<p style='color:red'> Resposta Incorreta. </p>")
-    else:
-        return HttpResponse("<p style='color:yellow'> Selecione uma alternativa. </p>")
