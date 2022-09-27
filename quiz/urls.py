05e5dd82ef7e5be252b7e77e0c7c1c28e67a8f5f
academiapython <academiapython.py@gmail.com>
2022-09-27 11:49:00 -0300

    Commitando arquivos antes de mudança de branch

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_quiz, name='get_quiz'),
+    path('quiz/resposta/', views.get_resposta, name='resposta')
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -6,9 +6,21 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 def get_quiz(request):
-    questao = Questao.objects.get(pk=3)
+    #questao = Questao.objects.raw('select * from blog.blog_db.quiz_questao order by random() limit 1')[0]
+    questao = Questao.objects.order_by("?").first()
     context = {
         'questao': questao
     }
     return render(request, 'quiz/quiz.html', context)
 
+
+def get_resposta(request):
+
+    resposta = request.POST.get('resposta')
+
+    if resposta == 'True':
+        return HttpResponse("<p style='color:green'> Resposta Correta! </p>")
+    elif resposta == 'False':
+        return HttpResponse("<p style='color:red'> Resposta Incorreta. </p>")
+    else:
+        return HttpResponse("<p style='color:red'> Selecione uma alternativa. </p>")
