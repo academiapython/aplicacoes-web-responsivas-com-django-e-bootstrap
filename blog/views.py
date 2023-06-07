dbb013a36c9018a7c88cdcea30f0ea6cc7537223
tiagobfaustino <tiagobfaustino@gmail.com>
2023-06-07 11:32:04 -0300

    Commit Reinicializando o curso.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+
 # Create your views here.
-
-
 def home(request):
-    posts = Post.objects.all().order_by('id')
-    context = {
-        'posts': posts
-    }
-    return render(request, 'blog/home.html', context)
+    return HttpResponse("<h1>Hello, World!!!</h1>")
--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
-"""core URL Configuration
+"""
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path("", include("blog.urls")),
 ]
